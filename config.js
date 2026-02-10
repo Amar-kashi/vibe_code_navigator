9548b3f9905f409eb7b38a8e207bc3a2ee3ec812
Amar-kashi <ksrct.amar.kashi@gmail.com>
2026-02-10 14:32:05 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Store your secrets here
-const CONFIG = {
-    // Paste your REAL Gemini Key here
-    AI_API_KEY: "AIzaSyBJw2FWrJci2Tp8HMEgUNL2_JNSmkFS5Og",
-    
-    // Paste your MapTiler Key here
-    MAPTILER_KEY: "iOISWWcR8S4iGHWRjAOZ"
-};  
